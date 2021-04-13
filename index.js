248b533e92b8830c68600bbe28d03817b3eb2d92
Jake Novelli <jakenovelli11@gmail.com>
2021-04-12 20:46:20 -0700

    added inquirer, fs and path. along with start of questions

---
+++ index.js
@@ -0,0 +1,53 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'Whats your GitHub username?',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is your projects name',
+        name: 'project'
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT', 'BSD_3', 'GPL_3.0', 'None']
+    }
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(path.join(__dirname, fileName), data, function(err){
+        if(err)throw err
+        console.log('File Written');
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        console.log(answers);
+        const data = generateMarkdown(answers);
+        writeToFile('README.md', data);
+    }).catch(err => console.log(err))
+}
+
+// Function call to initialize app
+init();
