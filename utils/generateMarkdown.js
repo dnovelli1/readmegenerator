caa34f0114348726f3effc341ab35102788e3563
Jake Novelli <jakenovelli11@gmail.com>
2021-04-14 19:20:07 -0700

    added functions for each license render. Added start to written string

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == 'None') {
+    return '';
+  } else {
+    var newLicense = license.split(' ').join('_');
+    return `![Badge](https://img.shields.io/badge/license-${newLicense}-blue)`;
+  } 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license == 'None'){
+    return '';
+  } else {
+    var newLicense = license.split(' ').join('-');
+    // license.toLowerCase();
+    return `https://choosealicense.com/licenses/${newLicense}/`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license == 'None'){
+    return '';
+  }else{
+    return `
+## License
+
+This project is currently licensed under the ${license} [License-Link](${renderLicenseLink(license).toLowerCase()})
+  `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-# Description
-${data.description}
+  return `
+# ${data.project}
+
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+
+*[Description](#Description)
+
+*[Installation](#Installation)
+
+*[Personal-Links](#Personal)
+
+
+
+## Description
+
+  ${data.description}
+
+
+
+## Personal Links for Business Inquiries
+
+[GitHub](https://github.com/${data.github})
+
+[Email-Me](${data.email})
+
+
 
 ${renderLicenseSection(data.license)}
+
 `;
 }
 
